701e8e26acf124d5c2a564b6259442b1cf6b0fc4
Kaique <kaiqueoliveira375@gmail.com>
2023-02-27 22:41:31 -0300

    Atualizando

--- academico/src/main/java/com/fieb/academico/model/User.java
+++ academico/src/main/java/com/fieb/academico/model/User.java
@@ -3,6 +3,7 @@ package com.fieb.academico.model;
 import java.util.Collection;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
@@ -21,7 +22,9 @@ public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	@Column(name="first_name")
 	private String firstName;
+	@Column(name="last_name")
 	private String lastName;
 	private String email;
 	private String password;
