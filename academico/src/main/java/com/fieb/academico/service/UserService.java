1e79d2026ba8746ab0ace80bfba17f18bcc5ad22
Kaique <kaiqueoliveira375@gmail.com>
2023-03-13 22:08:36 -0300

    Atualizando segurança do cadastro

--- academico/src/main/java/com/fieb/academico/service/UserService.java
+++ academico/src/main/java/com/fieb/academico/service/UserService.java
@@ -2,6 +2,13 @@ package com.fieb.academico.service;
 
 import org.springframework.security.core.userdetails.UserDetailsService;
 
+import com.fieb.academico.model.User;
+import com.fieb.academico.web.dto.UserDto;
+
 public interface UserService extends UserDetailsService{
 
+	User save (UserDto userDto);
+	User findByEmail(UserDto userDto);
+	User update (User user);
+	
 }
---
+++ academico/src/main/java/com/fieb/academico/web/UserController.java
@@ -0,0 +1,29 @@
+package com.fieb.academico.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import com.fieb.academico.service.UserService;
+import com.fieb.academico.web.dto.UserDto;
+
+@Controller
+public class UserController {
+
+	private UserService userService;
+
+	public UserController(UserService userService) {
+		this.userService = userService;
+	}
+
+	@GetMapping("/registration")
+	public String showRegistrationForm() {
+		return "registration";
+	}
+
+	public String registerUserAccount(@ModelAttribute("user") UserDto userDto) {
+		userService.save(userDto);
+		return "redirect:/registration?sucess";
+
+	}
+}
