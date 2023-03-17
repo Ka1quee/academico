b1452a84ba16fa50f109b656dbe4a52aad7405cb
Kaique <kaiqueoliveira375@gmail.com>
2023-03-16 22:20:45 -0300

    Adicionando cadastro de usuario

--- academico/src/main/java/com/fieb/academico/config/SecurityConfiguration.java
+++ academico/src/main/java/com/fieb/academico/config/SecurityConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -18,25 +19,30 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private UserService userService;
 
-		@Bean
-		public BCryptPasswordEncoder passwordEncoder() {
-			return new BCryptPasswordEncoder();
-		}
-		
-		public DaoAuthenticationProvider authenticationProvider() {
-			DaoAuthenticationProvider auth = new DaoAuthenticationProvider();
-				auth.setUserDetailsService(userService);
-				auth.setPasswordEncoder(passwordEncoder());
-				return null;
-			
-		}
-
-		@Override
-		protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-			// TODO Auto-generated method stub
-			super.configure(auth);
-		}
-		
-		
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 
+	public DaoAuthenticationProvider authenticationProvider() {
+		DaoAuthenticationProvider auth = new DaoAuthenticationProvider();
+		auth.setUserDetailsService(userService);
+		auth.setPasswordEncoder(passwordEncoder());
+		return auth;
+
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.authenticationProvider(authenticationProvider());
+		super.configure(auth);
+	}
+
+	@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.authorizeRequests().antMatchers(
+					"registration**").permitAll();
+
+		}
+
+}
